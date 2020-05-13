980dcb3af40d1934d40233f5a39f077fee40cb5c
David Torralbo <davidtorralboperez@gmail.com>
2020-05-14 01:10:29 +0200

    Fix vue warning by including script on form

--- resources/views/captcha.blade.php
+++ resources/views/captcha.blade.php
@@ -1,6 +1,6 @@
 @if(config('simple-recaptcha-v3.active'))
     <input type="hidden" name="recaptcha_response">
-    <script>
+    <script type="application/javascript">
         grecaptcha.ready(function() {
             grecaptcha.execute("{{ config('simple-recaptcha-v3.site_key') }}", {action: 'gromenauer'}).then(function (token) {
                 let recaptchaResponse = document.getElementsByName('recaptcha_response')[0]
