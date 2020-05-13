083338c1dec11a0fd8acd3d7c3a06b708faef573
David Torralbo <davidtorralboperez@gmail.com>
2020-05-14 01:10:14 +0200

    Add option to hide the badge and option to prefer navigator language instead of app one

--- config/config.php
+++ config/config.php
@@ -5,4 +5,10 @@ return [
     'site_key' => env('CAPTCHA_SITE_KEY', ''),
     'secret_key' => env('CAPTCHA_SECRET_KEY', ''),
     'minimum_score' => env('CAPTCHA_MINIMUM_SCORE', 0.5),
+
+    /*
+     * Before hide badge, you may read https://developers.google.com/recaptcha/docs/faq#id-like-to-hide-the-recaptcha-badge.-what-is-allowed
+     */
+    'hide_badge' => env('CAPTCHA_HIDE_BADGE', false),
+    'prefer_navigator_language' => env('CAPTCHA_PREFER_NAVIGATOR_LANGUAGE', false),
 ];
--- resources/views/captcha-api.blade.php
+++ resources/views/captcha-api.blade.php
@@ -1,3 +1,8 @@
 @if(config('simple-recaptcha-v3.active'))
-    <script src="https://www.google.com/recaptcha/api.js?render={{ config('simple-recaptcha-v3.site_key') }}"></script>
+    @if(config('simple-recaptcha-v3.hide_badge'))
+        <style>
+            .grecaptcha-badge { visibility: hidden; }
+        </style>
+    @endif
+    <script src="https://www.google.com/recaptcha/api.js?render={{ config('simple-recaptcha-v3.site_key') }}{{ (! config('simple-recaptcha-v3.prefer_navigator_language')) ? '&hl='.app()->getLocale() : '' }}"></script>
 @endif
