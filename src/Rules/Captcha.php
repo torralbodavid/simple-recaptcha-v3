34b3c152fb714befa2081872e52e286c27fe5598
David Torralbo <davidtorralboperez@gmail.com>
2020-05-14 00:37:04 +0200

    Add captcha rule

---
+++ src/Rules/Captcha.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Torralbodavid\SimpleRecaptchaV3\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use Torralbodavid\SimpleRecaptchaV3\Services\Captcha as CaptchaService;
+
+class Captcha implements Rule
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function passes($attribute, $value)
+    {
+        if (! config('simple-recaptcha-v3.active')) {
+            return true;
+        }
+
+        if ($value === null) {
+            return false;
+        }
+
+        $captcha = new CaptchaService($value);
+        $response = $captcha->getResponse();
+
+        if (! $response->success || $response->score < config('simple-recaptcha-v3.minimum_score')) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function message()
+    {
+        return 'simple-recaptcha-v3::messages.failed';
+    }
+}
