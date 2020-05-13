c6525a1552bc9cb79ca859773d30b72235ea0c0e
David Torralbo <davidtorralboperez@gmail.com>
2020-05-14 00:37:23 +0200

    Update config keys

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
 
-/*
- * You can place your custom package configuration in here.
- */
 return [
-
+    'active' => env('CAPTCHA_ENABLED', true),
+    'site_key' => env('CAPTCHA_SITE_KEY', ''),
+    'secret_key' => env('CAPTCHA_SECRET_KEY', ''),
+    'minimum_score' => env('CAPTCHA_MINIMUM_SCORE', 0.5),
 ];
