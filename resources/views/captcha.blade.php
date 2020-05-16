bf643a739c484dab40914dfa82897f16a3693a6e
David Torralbo <davidtorralboperez@gmail.com>
2020-05-16 13:13:18 +0200

    Call prepareCaptcha when page loads

--- resources/views/captcha.blade.php
+++ resources/views/captcha.blade.php
@@ -1,11 +1,9 @@
 @if(config('simple-recaptcha-v3.active'))
     <input type="hidden" name="recaptcha_response">
     <script type="application/javascript">
-        grecaptcha.ready(function() {
-            grecaptcha.execute("{{ config('simple-recaptcha-v3.site_key') }}", {action: 'gromenauer'}).then(function (token) {
-                let recaptchaResponse = document.getElementsByName('recaptcha_response')[0]
-                recaptchaResponse.value = token
-            })
-        });
+        window.onload = function() {
+            let form = document.getElementsByName("recaptcha_response")[0].closest('form')
+            prepareCaptcha({{ $action }}, form)
+        }
     </script>
 @endif
