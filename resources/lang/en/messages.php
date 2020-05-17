2bfff10f2d0014980fc6215d55b4529da224d1a6
David Torralbo <davidtorralboperez@gmail.com>
2020-05-17 12:09:10 +0200

    Add new error messages

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -1,5 +1,10 @@
 <?php
 
 return [
-    'failed' => 'Captcha is not valid'
+    'missing-input-secret' => 'The secret parameter is missing.',
+    'invalid-input-secret' => 'The secret parameter is invalid or malformed.',
+    'missing-input-response' => 'The response parameter is missing.',
+    'invalid-input-response' => 'The response parameter is invalid or malformed.',
+    'bad-request' => 'The request is invalid or malformed.',
+    'timeout-or-duplicate' => 'The response is no longer valid: either is too old or has been used previously.'
 ];
