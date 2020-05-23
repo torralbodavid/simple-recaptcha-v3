5c97e3d6ad19d759cda52fbcbcac2d6966d327d8
David Torralbo <davidtorralboperez@gmail.com>
2020-05-23 16:12:01 +0200

    Make captcha service an invocable class

--- src/Services/Captcha.php
+++ src/Services/Captcha.php
@@ -8,14 +8,11 @@ class Captcha
     protected $secret;
     protected $response;
 
-    public function __construct($recaptcha_response)
+    public function __invoke($recaptcha_response)
     {
         $this->secret = config('simple-recaptcha-v3.secret_key');
         $this->response = $recaptcha_response;
-    }
 
-    public function getResponse()
-    {
         $recaptcha = file_get_contents(self::SERVICE_URL.'?secret='.$this->secret.'&response='.$this->response);
 
         return json_decode($recaptcha);
