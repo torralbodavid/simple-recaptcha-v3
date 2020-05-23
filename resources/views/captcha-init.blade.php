ab2ec49a97a894bfebd095cd459d091d3f21f460
David Torralbo <davidtorralboperez@gmail.com>
2020-05-23 18:41:46 +0200

    Load recaptcha api asynchronously

--- resources/views/captcha-init.blade.php
+++ resources/views/captcha-init.blade.php
@@ -2,7 +2,7 @@
     @if(config('simple-recaptcha-v3.hide_badge'))
         <style> .grecaptcha-badge { visibility: hidden; } </style>
     @endif
-    <script src="https://www.google.com/recaptcha/api.js?render={{ config('simple-recaptcha-v3.site_key') }}{{ (! config('simple-recaptcha-v3.prefer_navigator_language')) ? '&hl='.app()->getLocale() : '' }}"></script>
+    <script async defer src="https://www.google.com/recaptcha/api.js?render={{ config('simple-recaptcha-v3.site_key') }}{{ (! config('simple-recaptcha-v3.prefer_navigator_language')) ? '&hl='.app()->getLocale() : '' }}"></script>
     <script type="application/javascript">
         function prepareCaptcha(action, id) {
             grecaptcha.ready(() => {
--- resources/views/captcha.blade.php
+++ resources/views/captcha.blade.php
@@ -2,6 +2,8 @@
     @php $id = app('simple-recaptcha-v3')->generateId() @endphp
     <input type="hidden" name="recaptcha_response" id="{{ $id }}">
     <script type="application/javascript">
-        prepareCaptcha('{{ $action }}', '{{ $id }}')
+        document.addEventListener("DOMContentLoaded", function(event) {
+            prepareCaptcha('{{ $action }}', '{{ $id }}')
+        });
     </script>
 @endif
