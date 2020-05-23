47662532ba174ccfd29c034c40cfb3e856cf34ae
David Torralbo <davidtorralboperez@gmail.com>
2020-05-23 16:11:02 +0200

    Add new error messages

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -1,10 +1,12 @@
 <?php
 
 return [
-    'missing-input-secret' => 'The secret parameter is missing.',
-    'invalid-input-secret' => 'The secret parameter is invalid or malformed.',
-    'missing-input-response' => 'The response parameter is missing.',
-    'invalid-input-response' => 'The response parameter is invalid or malformed.',
-    'bad-request' => 'The request is invalid or malformed.',
-    'timeout-or-duplicate' => 'The response is no longer valid: either is too old or has been used previously.'
+    'missing-input-secret' => 'The secret captcha parameter is missing.',
+    'invalid-input-secret' => 'The secret captcha parameter is invalid or malformed.',
+    'missing-input-response' => 'The captcha response parameter is missing.',
+    'invalid-input-response' => 'The captcha response parameter is invalid or malformed.',
+    'bad-request' => 'The captcha request is invalid or malformed.',
+    'timeout-or-duplicate' => 'The captcha response is no longer valid: either is too old or has been used previously.',
+    'response-null' => 'The captcha response seems to be null.',
+    'hostname-error' => 'The hostname is incorrect.'
 ];
